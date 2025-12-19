76c17b528395b2c0013f0f8020257f88e33e7d22
StateSource <167351918+StateSource@users.noreply.github.com>
2025-12-19 21:35:12 +0100

    JS main random string generator for main app API
    JS main random string generator for main app API

--- services/generation module.js
+++ services/generation module.js
@@ -1,26 +1,57 @@
-class Random12Extension {
+class RandomCodeADV {
     getInfo() {
         return {
-            id: 'random12',
-            name: 'ADV Variables',
+            id: 'randomcodeadv',
+            name: 'Random Code ADV',
             blocks: [
                 {
                     opcode: 'generate',
                     blockType: Scratch.BlockType.REPORTER,
-                    text: 'générer code aléatoire (12 caractères)'
+                    text: 'générer code aléatoire longueur [LEN]',
+                    arguments: {
+                        LEN: {
+                            type: Scratch.ArgumentType.NUMBER,
+                            defaultValue: 12
+                        }
+                    }
+                },
+                {
+                    opcode: 'generateCustom',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: 'générer code [MIN] [MAJ] [NUM] longueur [LEN]',
+                    arguments: {
+                        MIN: { type: Scratch.ArgumentType.BOOLEAN, defaultValue: true },
+                        MAJ: { type: Scratch.ArgumentType.BOOLEAN, defaultValue: false },
+                        NUM: { type: Scratch.ArgumentType.BOOLEAN, defaultValue: true },
+                        LEN: { type: Scratch.ArgumentType.NUMBER, defaultValue: 12 }
+                    }
                 }
             ]
         };
     }
 
-    generate() {
-        const chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
-        let result = '';
-        for (let i = 0; i < 12; i++) {
-            result += chars.charAt(Math.floor(Math.random() * chars.length));
+    generate(args) {
+        return this._gen(true, false, true, args.LEN);
+    }
+
+    generateCustom(args) {
+        return this._gen(args.MIN, args.MAJ, args.NUM, args.LEN);
+    }
+
+    _gen(min, maj, num, len) {
+        let chars = '';
+        if (min) chars += 'abcdefghijklmnopqrstuvwxyz';
+        if (maj) chars += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        if (num) chars += '0123456789';
+
+        if (!chars || len <= 0) return '';
+
+        let out = '';
+        for (let i = 0; i < len; i++) {
+            out += chars[Math.floor(Math.random() * chars.length)];
         }
-        return result;
+        return out;
     }
 }
 
-Scratch.extensions.register(new Random12Extension());
+Scratch.extensions.register(new RandomCodeADV());
